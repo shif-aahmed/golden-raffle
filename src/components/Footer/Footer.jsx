564b3e00ed6940b1b3232a7dfd2dd648cb0bf84c
Shifa Ahmed <shifaahmed3108@gmail.com>
2025-10-27 11:21:17 +0500

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{
+      backgroundColor: '#333',
+      color: 'white',
+      textAlign: 'center',
+      padding: '20px',
+      marginTop: 'auto'
+    }}>
+      <p>&copy; 2024 Golden Raffle. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
