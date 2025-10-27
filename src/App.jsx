6dfcfcf82e5ce10e16820a1dcb98088880384d9f
Shifa Ahmed <shifaahmed3108@gmail.com>
2025-10-27 12:18:03 +0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-
+import Navbar from './components/Navbar/Navbar'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
