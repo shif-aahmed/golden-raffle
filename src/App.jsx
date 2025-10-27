3529eab009bb95671552e2aaebcd617fd384f5ce
Shifa Ahmed <shifaahmed3108@gmail.com>
2025-10-27 12:39:16 +0500

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   )
 }
